e6c91a0a394abdc732acb5c931ce10d0952383d4
TKIDawn <teasmc@163.com>
2023-02-08 08:19:47 +0800

    Summary

--- AcFun/Program.cs
+++ AcFun/Program.cs
@@ -2,10 +2,25 @@
 {
     class Program
     {
+        private static string _version = "1.0";
         static void Main() => new Program().MainAsync();
         async void MainAsync()
         {
-            new WebApi().Start();
+            Util.Print("AcFun视频信息解析软件 v"+_version);
+            Util.Print("作者：初雪 OriginalSnow");
+            Util.Print("项目地址：https://github.com/IM2eR0/AcFun-VideoInfo-API");
+            Console.WriteLine();
+
+            Util.Print("正在启动 HttpListener 监听...");
+
+            try
+            {
+                new WebApi().Start();
+            }
+            catch(Exception ex)
+            {
+                Util.Print(ex.Message, Util.PrintType.ERROR);
+            }
         }
     }
 }
